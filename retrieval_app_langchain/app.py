85a96fb1ef1e05397ee7634f5991b898ed1a8536
stanislavlia <stanislavliashkov@gmail.com>
2024-05-15 15:17:39 +0700

    Added metadata to results

--- retrieval_app_langchain/app.py
+++ retrieval_app_langchain/app.py
@@ -2,11 +2,16 @@ from retrieval import get_vecstore_client, load_and_split_doc, add_chunks_to_db
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pydantic import BaseModel, Field
+from typing import Optional, Dict, List
+from langchain.schema import Document
 
 from fastapi import FastAPI, status, File, UploadFile, HTTPException
 import logging
 import os
 import datetime
+import json
+
 
 STORAGE_DIR_PATH="/home/stanislav/Desktop/ft_search/retrieval_app_langchain/tmp"
 
@@ -20,6 +25,13 @@ embedding_func = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 vectorstore = get_vecstore_client(embedding_func=embedding_func)
 
 app = FastAPI()
+
+
+#Pydantic models
+class QuerySchema(BaseModel):
+	query : str
+	n_results : int
+
 
 
 @app.get("/")
@@ -39,6 +51,7 @@ def load_pdf_to_vectore(file : UploadFile = File(...)):
     if file.content_type != 'application/pdf':
          return {"message": "This endpoint accepts only PDF files."}
     
+
     content = file.file.read()
     file_path = os.path.join(STORAGE_DIR_PATH, file.filename)
 
@@ -51,17 +64,20 @@ def load_pdf_to_vectore(file : UploadFile = File(...)):
                      chunks=chunks)
     return {"message" : f"{len(chunks)} chunks of {file.filename} were added"}
 
+
+@app.post("/query")
+def query_docs(query : QuerySchema):
     
-
-
-# chunks = load_and_split_doc(path="/home/stanislav/Desktop/ft_search/retrieval_app_langchain/LLMs are few shot learners.pdf",
-#                             text_splitter=text_splitter)
-
-# print("Loaded chunks ", len(chunks))
-
-# add_chunks_to_db(langchain_chromadb_vecstore=vectorstore,
-#                  chunks=chunks)
-
-# print(vectorstore.max_marginal_relevance_search(query="Math problems solving", k=2))
+    retrieved_docs = vectorstore.max_marginal_relevance_search(query=query.query,
+                                                     k=query.n_results,
+                                                     fetch_k=(query.n_results * 2),
+                                                     )
+    
+    result = {"chunks_retrieved" : len(retrieved_docs),
+              "docs" : [doc.page_content for doc in retrieved_docs],
+              "metadatas" : [doc.metadata for doc in retrieved_docs]}
+    
+    
+    return result
 
 
